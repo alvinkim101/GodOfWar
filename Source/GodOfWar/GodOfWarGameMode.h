d7dc1780687e34590412becdcbdf7164047e5b43
Alvin Kim <alvinkim101@gmail.com>
2024-03-19 01:50:16 -0700

    Add third person template

---
+++ Source/GodOfWar/GodOfWarGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "GodOfWarGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AGodOfWarGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AGodOfWarGameMode();
+};
+
+
+
